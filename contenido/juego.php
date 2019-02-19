7081d986e4d99373b56ac5abe6426850b1f12905
Carlos Segura Antón <peleino_pilas@hotmail.com>
2019-02-19 11:17:29 +0100

    Add files via upload

--- contenido/juego.php
+++ contenido/juego.php
@@ -20,3 +20,17 @@
         <div class="barco5"></div>
     </body>
 </html>
+
+<?php
+
+error_reporting(E_ALL);
+
+require_once '../Model/Usuario.php';
+
+// Obtiene todos los usuarios
+$data['usuarios'] = Usuario::getUsuarios();
+
+// Carga la vista de listado
+include '../View/ListUsuarios.php';
+
+?>
