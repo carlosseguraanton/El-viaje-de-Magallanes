d3fb86772c5aad87adb6283a7ab3a94ad975713f
Carlos Segura Antón <peleino_pilas@hotmail.com>
2019-02-21 09:48:51 +0100

    Add files via upload

--- contenido/controller/juego.php
+++ contenido/controller/juego.php
@@ -7,19 +7,29 @@
 
 <html>
     <head>
+
         <meta charset="UTF-8"/>
+
         <title>El viaje de Magallanes</title>
+
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <link rel="shortcut icon" href="./../../img/magallanes.jpg">
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
         <script type="text/javascript" src="./../../js/main.js"></script>
+
         <link rel="stylesheet" type="text/css" href="./../../css/main.css">
+
     </head>
     <body>
+
         <div class="divImg">
             <img src="./../../img/juego.png">
         </div>
+
         <?php
         
             $br = "<br>";
@@ -64,6 +74,7 @@
 
 
         ?>
+        
     </body>
 </html>
 
