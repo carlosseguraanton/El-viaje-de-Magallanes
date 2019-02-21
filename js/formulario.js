1f0feaf7865665eef510e3158e3927478c331b74
Carlos Segura Antón <peleino_pilas@hotmail.com>
2019-02-21 09:44:30 +0100

    Add files via upload

--- js/formulario.js
+++ js/formulario.js
@@ -15,7 +15,7 @@ window.onload = function() {
 			if ( elementos[ i ].type == "text" ) {
 
 				// Si es tipo texto vamos a comprobar que esten completados los input
-				if ( elementos[i].value.length == 0 ) {
+				if ( elementos[ i ].value.length == 0 ) {
 
 					console.log( 'El campo ' + elementos[ i ].name + ' esta incompleto' );
 
@@ -66,9 +66,11 @@ window.onload = function() {
 	var blurInput = function() {
 
 		if ( this.value <= 0 ) {
+
 			this.parentElement.children[ 1 ].className = "label";
 
 			this.parentElement.children[ 0 ].className = this.parentElement.children[ 0 ].className + " error";
+			
 		}
 
 	};
